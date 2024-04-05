2ff523c4ba2121fd6c8ffdd8f0ee13639d91a492
Anastasiia Levchenko <nastiya.levchenko@gmail.com>
2024-04-05 08:22:51 +0200

    Added Read File Function

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def read_file(file_name):
     with open(file_name, 'r') as file:
         return set(file.read().splitlines())
+    
+def write_to_file(file_name, lines):
+    with open(file_name, 'w') as file:
+        file.write('\n'.join(lines))
